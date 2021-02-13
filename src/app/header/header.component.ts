81aa57e3ac28ea123f521ef8860d735ff3503d52
Brendan Vavra <brendan.vavra@gmail.com>
2021-02-13 08:55:05 -0500

    Got class change to work when scrolling down (not so much when scrolling up yet though)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -10,10 +11,20 @@ export class HeaderComponent implements OnInit {
   title = 'Brendan Vavra';
   url = 'http://bvavra.github.io';//TODO - do I want to link directly to the URL 'http://bvavra.github.io', or do I want it to smart nav to Home like the nav menu? 
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) private document: Document) { }
 
   ngOnInit(): void {
     this.title = 'Brendan Vavra';
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    // if (document.body.scrollTop <= 50 || document.documentElement.scrollTop <= 50) {
+    //   document.getElementById('navbar').classList.remove('top-nav-short');
+    // } else 
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      document.getElementById('navbar').classList.add('top-nav-short');
+    }
   }
 
 }
