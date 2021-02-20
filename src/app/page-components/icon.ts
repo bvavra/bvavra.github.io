f09f9e86cfab4dd3767adf65fdff199cc5ba8df3
Brendan Vavra <brendan.vavra@gmail.com>
2021-02-20 07:29:46 -0500

    Auto-generated commit

--- src/app/page-components/icon.ts
+++
@@ -1,7 +0,0 @@
-export interface Icon {
-    id: number;    //Currently unused, but if I move icons to service/db we'll need a PK
-    title: string; //Tooltip
-    link: string;  //Href Link 
-    class: string; //Which font-awesome icon to use
-    style: string; //Which font-awesome style the icon comes from (fa = Regular, fab = Brand, fas = Solid)
-}
