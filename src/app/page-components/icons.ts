f3a4cdfadc5f31db9d5aca911a1c43497e76e8e3
Brendan Vavra <brendan.vavra@gmail.com>
2021-02-27 09:54:49 -0500

    Add icon for SoundCloud; add some embedded original composition SoundCloud tracks

--- src/app/page-components/icons.ts
+++ src/app/page-components/icons.ts
@@ -3,9 +3,10 @@ import { Icon } from './icon';
 export const ICONS: Icon[] = [
   { id: 1, class: 'fa-github', link: 'https://github.com/bvavra', title: 'GitHub', style: 'fab' },
   { id: 2, class: 'fa-youtube', link: 'https://www.youtube.com/c/BrendanVavra', title: 'YouTube', style: 'fab'},
-  { id: 3, class: 'fa-twitter', link: 'https://twitter.com/brendanvavra', title: 'Twitter', style: 'fab' },
-  { id: 4, class: 'fa-instagram', link: 'https://www.instagram.com/brendanvavra', title: 'Instagram', style: 'fab' },
-  { id: 5, class: 'fa-envelope', link: 'mailto:brendan.vavra@gmail.com', title: 'Email me', style: 'fa' }
+  { id: 3, class: 'fa-soundcloud', link: 'https://soundcloud.com/brendanvavra', title: 'SoundCloud', style: 'fab'},
+  { id: 4, class: 'fa-twitter', link: 'https://twitter.com/brendanvavra', title: 'Twitter', style: 'fab' },
+  { id: 5, class: 'fa-instagram', link: 'https://www.instagram.com/brendanvavra', title: 'Instagram', style: 'fab' },
+  { id: 6, class: 'fa-envelope', link: 'mailto:brendan.vavra@gmail.com', title: 'Email me', style: 'fa' }
 ];
 
 //Look for more icons here: https://fontawesome.com/icons?d=gallery&m=free
