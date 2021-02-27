2c82b74a1d49081313d955e89f84163d769e3ce7
Brendan Vavra <brendan.vavra@gmail.com>
2021-02-27 11:28:51 -0500

    Add Linked In icon to footer

--- src/app/page-components/icons.ts
+++ src/app/page-components/icons.ts
@@ -2,11 +2,12 @@ import { Icon } from './icon';
 
 export const ICONS: Icon[] = [
   { id: 1, class: 'fa-github', link: 'https://github.com/bvavra', title: 'GitHub', style: 'fab' },
-  { id: 2, class: 'fa-youtube', link: 'https://www.youtube.com/c/BrendanVavra', title: 'YouTube', style: 'fab'},
-  { id: 3, class: 'fa-soundcloud', link: 'https://soundcloud.com/brendanvavra', title: 'SoundCloud', style: 'fab'},
-  { id: 4, class: 'fa-twitter', link: 'https://twitter.com/brendanvavra', title: 'Twitter', style: 'fab' },
-  { id: 5, class: 'fa-instagram', link: 'https://www.instagram.com/brendanvavra', title: 'Instagram', style: 'fab' },
-  { id: 6, class: 'fa-envelope', link: 'mailto:brendan.vavra@gmail.com', title: 'Email me', style: 'fa' }
+  { id: 2, class: 'fa-linkedin-in', link: 'https://www.linkedin.com/in/brendan-vavra/', title: 'Linked In', style: 'fab' },
+  { id: 3, class: 'fa-youtube', link: 'https://www.youtube.com/c/BrendanVavra', title: 'YouTube', style: 'fab'},
+  { id: 4, class: 'fa-soundcloud', link: 'https://soundcloud.com/brendanvavra', title: 'SoundCloud', style: 'fab'},
+  { id: 5, class: 'fa-twitter', link: 'https://twitter.com/brendanvavra', title: 'Twitter', style: 'fab' },
+  { id: 6, class: 'fa-instagram', link: 'https://www.instagram.com/brendanvavra', title: 'Instagram', style: 'fab' },
+  { id: 7, class: 'fa-envelope', link: 'mailto:brendan.vavra@gmail.com', title: 'Email me', style: 'fa' }
 ];
 
 //Look for more icons here: https://fontawesome.com/icons?d=gallery&m=free
